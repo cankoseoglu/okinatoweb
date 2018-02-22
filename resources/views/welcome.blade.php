050f6ab14645098cac8033c870f932b276a92a3b
Furkan Tasel <furkantasel@gmail.com>
2018-02-22 15:54:34 +0300

    laravel init w theme

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Basic Page Info -->
+    <meta charset="utf-8">
+    <title>Okinato - Fast, Secure Financial Exchange</title>
+    <meta name="description" content="Okinato - Fast, Secure Financial Exchange">
+    <meta name="author" content="okinato.comm">
+
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="img/fav.png" />
+
+    <!-- FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Libre+Baskerville:400,700" rel="stylesheet">
+
+    <!-- ICON FONTS -->
+    <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/lightbox.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+<div class="loader_bg">
+    <div class="loader"></div>
+</div>
+<div class="wrapper">
+    <div class="bg_img" id="bg_triangles"></div>
+    <div class="background_dots"></div>
+    <div class="left color_white hideee">
+        <div class="brand"><img src="" class="img-responsive" alt=""></div>
+        <div class="content">
+            <h1 class="big-title font_serif ssa js-spanize">Okinato</h1>
+            <h2 class="font_serif">
+                Putting
+                <span class="text_bordered sa js-spanize">Customers</span> <br>
+                First </h2>
+            <p>Your new go-to place for buying and selling Bitcoin, Ethereum and other select digital assets. Trading made fast, easy and secure.</p>
+            <div class="space_5 hidden-xs"></div>
+            <div class="btns">
+                <div class="btn background_green2 shw" data-target="subscribe_section"><i class="fa fa-paper-plane-o"></i> Get notified</div>
+                <div class="btn bordered shw" data-target="right"><i class="fa fa-angle-double-right"></i> Explore</div>
+            </div>
+        </div>
+    </div>
+    <div class="right">
+        <div class="close-mob"><i class="fa fa-close"></i></div>
+        <div id="about">
+            <h2 class="title font_bold text_bordered">About us</h2>
+            <div class="space_5"></div>
+            <p>Okinato is a brand new global cryptocurrency exchange and trading platform.</p>
+            <p>Offering a cutting-edge trading engine, liquid orderbook, user-friendly interface, and high security for users’ funds, Okinato makes digital asset trading fast, easy, and secure.</p>
+            <p> Whether you are a seasoned trader, an institutional investor, or an individual thinking of investing in digital assets for the first time, Okinato will be your new and prime go-to place for buying and selling Bitcoin, Ethereum, and other select cryptocurrencies. </p>
+            <div class="space_5"></div>
+            <div class="services">
+                <div class="col-xs-12 col-sm-6 service">
+                    <div class="icon">
+                        <img src="icons/watercolor.png" alt="">
+                    </div>
+                    <h4>High Performance Platform</h4>
+                    <p> Okinato’s custom-built stable platform offers high liquidity, different order types, realtime orderbooks, a number of charting tools and much more to facilitate and enhance your trading experience.
+                    </p>
+                </div>
+                <div class="col-xs-12 col-sm-6 service">
+                    <div class="icon">
+                        <img src="icons/layers.png" alt="">
+                    </div>
+                    <h4>Security First</h4>
+                    <p>Placing paramount importance on the safety of user funds, Okinato utilizes sophisticated encryption on all its wallets, and stores over 95% of its users’ digital assets offline, using a top-tier cold storage solution.
+                    </p>
+                </div>
+            </div>
+          
+        <div id="contact">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="contact">
+                        <h2 class="font_bold title text_bordered">Get in touch</h2>
+                        <br>
+                        <p> We are currently expanding our team, and hiring on multiple fronts. If you are interested in joining Okinato, you represent a business that would like to cooperate with Okinato, or you simply have ideas or suggestions, please reach out to us by email. </p>
+                   
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="space_10"></div>
+                        <div class="item">
+                            <img src="icons/map.png" class="img-responsive" alt="">
+                            <p class="color_grey font_bold"></p>
+                        </div>
+                        <div class="item">
+                            <img src="icons/envelope.png" class="img-responsive" alt="">
+                            <p class="color_grey font_bold"><a href="mailto:hello@okinato.com">hello@okinato.com</a></p>
+                        </div>
+                        <div class="item">
+                            <img src="icons/telephone.png" class="img-responsive" alt="">
+                            <p class="color_grey font_bold"><a href="tel:+00 123 456 789" class="letter-spacing_1 font_bold"></a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="subscribe_section">
+        <div class="close-mob"><i class="fa fa-close"></i></div>
+        <div class=" background_green2 overlay_black_75"></div>
+        <div class="d_table">
+            <div class="d_table_v">
+                <h1 class="font_bold">Get notified!</h1>
+                <p>Leave us your details and we will let you know when we have launched.</p>
+                <div class="space_10"></div>
+                <form class="subscribe" action="subscribe.php" method="POST">
+                    <div class="form-group">
+                        <div class="controls">
+                            <!-- Field  -->
+                      <p> send us an email at hello@okinato.com </p>
+                        </div>
+                    </div>
+                </form>
+                <div class="space_15"></div>
+            </div>
+        </div>
+    </div>
+    <div class="social oop">
+        <ul class="list-inline   list-inline">
+            <li><a href="#"><i class="fa fa-facebook-official"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+        </ul>
+    </div>
+    <div class="copyright text-right oop">
+        <b>OKINATO</b> <br>
+         hello@okinato.com <br>
+    Copyright © 2018 Okinato.com - All Rights Reserved.
+    </div>
+    <div class="close-ss"></div>
+</div>
+
+
+<script type="text/javascript" src="{{asset('js/jquery-3.1.1.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/contact.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/validator.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/triangles.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/lightbox.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/script.js')}}"></script>
+</body>
+</html>
